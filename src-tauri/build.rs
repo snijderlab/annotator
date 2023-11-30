6db7d548f77b903e8faee6559a6e4b52e977bcf6
Douwe Schulte <d.schulte@uu.nl>
2023-11-30 20:19:49 +0100

    Slightly better

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,7 +12,6 @@ r#"<!DOCTYPE html>
 
 <head>
   <meta charset="UTF-8" />
-  <link rel="stylesheet" href="stitch-assets/styles.css" />
   <link rel="stylesheet" href="style.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Annotator</title>
